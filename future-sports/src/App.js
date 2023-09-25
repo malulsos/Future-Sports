d630ab6f88f4fbc5851a296123dbcf891a3d9c42
malulsos <malulsos@hotmail.com>
2023-09-26 00:42:00 +1000

    build

--- future-sports/src/App.js
+++ future-sports/src/App.js
@@ -1,3 +1,4 @@
+// Import Navigation, Header, Main, Aside, and Footer components
 import React from 'react';
 import './App.css';
 import Navigation from "./Components/Navigation/Navigation";
@@ -6,18 +7,21 @@ import Main from "./Components/Main/Main";
 import Aside from "./Components/Aside/Aside";
 import Footer from "./Components/Footer/Footer";
 
+// Define the App component
 function App() {
+  // Render the App component
   return (
     <div className="App">
-      <Header />
-      <Navigation />
-      <div className="container">
-        <Main />
-        <Aside />
+      <Header /> {/* Include Header component */}
+      <Navigation /> {/* Include Navigation component */}
+      <div className="container"> {/* Container for Main and Aside components */}
+        <Main /> {/* Include Main component */} 
+        <Aside /> {/* Include Aside component */}
       </div>
-      <Footer />
+      <Footer /> {/* Include Footer component */}
     </div>
   );
 }
 
+// Export the App component
 export default App;
